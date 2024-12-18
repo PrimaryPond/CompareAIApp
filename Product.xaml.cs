7705fd392a05f9014c9b6fa641038a4140fd3670
PrimaryPond <eth458@gmail.com>
2024-12-18 12:42:57 -0600

    commiting current changes for break, working on custom controls

---
+++ Product.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompareAI
+{
+    /// <summary>
+    /// Interaction logic for Product.xaml
+    /// </summary>
+    public partial class Product : UserControl
+    {
+        private string productName;
+        public string ProductName
+        {
+            get { return productName; }
+            set { 
+                productName = value; 
+                tbProductName.Content = productName;
+                }
+        }
+        private string productDesc;
+        public string ProductDesc
+        {
+            get { return productDesc; }
+            set
+            {
+                productDesc = value;
+                tbProductDesc.Text = productDesc;
+            }
+        }
+        public Product()
+        {
+            InitializeComponent();
+        }
+    }
+}
